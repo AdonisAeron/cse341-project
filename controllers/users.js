b84792867931638fa9c3fbd6c2e837c88a021c8a
aregetlam <jon21099@byui.edu>
2024-05-12 01:35:27 -0400

    Error handling corrections.

--- controllers/users.js
+++ controllers/users.js
@@ -3,35 +3,37 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
     //#swagger.tags=['Contacts']
-    const result = await mongodb.getDb().db().collection('contacts').find();
-    /*result.toArray().then((contacts) => {
+    try {
+        const db = mongodb.getDb().db();
+        const contacts = await db.collection('contacts').find().toArray(); 
+
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts);
-    });*/
-    result.toArray((err, contacts) => {
-        if (err) {
-            res.status(400).json({message: err});
+    } catch (err) {
+        console.error(`Error: ${err}`);
+        
+        if (!res.headersSent) {
+            res.status(400).json({ message: 'Failed to retrieve contacts' });
         }
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts);
-    });
+    }
 };
 
 const getSingle = async (req, res) => {
     //#swagger.tags=['Contacts']
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
-    /*result.toArray().then((contacts) => {
+    try {
+        const userId = new ObjectId(req.params.id);
+        const db = mongodb.getDb().db();
+        const contacts = await db.collection('contacts').find({ _id: userId }).toArray();
+        
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts);
-    });*/
-    result.toArray((err, contacts) => {
-        if (err) {
-            res.status(400).json({message: err});
+    } catch (err) {
+        console.error(`Error: ${err}`);
+        
+        if (!res.headersSent) {
+            res.status(400).json({ message: 'Failed to retrieve that contact' });
         }
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts);
-    });
+    }
 };
 
 
