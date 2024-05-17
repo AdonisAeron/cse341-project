e19492c2c25dac2f6ecc74bf643680b6d2f753b9
aregetlam <jon21099@byui.edu>
2024-05-17 19:30:23 -0400

    Fixing authentication error

--- routes/auth.js
+++ routes/auth.js
@@ -3,17 +3,17 @@ const passport = require('passport');
 const session = require('express-session');
 const GitHubStrategy = require('passport-github2').Strategy;
 
-routes.get('/callback', passport.authenticate('github', { failureRedirect: '/api-docs' }), (req, res) => {
-    try {
-        if (!req.user) {
-            throw new Error('No user returned from GitHub');
-        }
+routes.get('/callback', passport.authenticate('github', {
+    failureRedirect: '/api-docs', session: false
+}), (req, res) => {
+    if (req.error) {
+        console.error("Authentication error: ", req.error);
+        res.status(500).send("Authentication error");
+    } else {
         req.session.user = req.user;
         res.redirect('/');
-    } catch (error) {
-        console.error('Callback Error:', error);
-        res.redirect('/api-docs');
     }
 });
+
 
 module.exports = routes;
